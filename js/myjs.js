288f369789bd4035630307f50e50f8a5d1d54700
Sai Prathima Parsa <saiprathima@users.noreply.github.com>
2015-03-15 13:20:07 -0700

    Update myjs.js

--- js/myjs.js
+++ js/myjs.js
@@ -1,8 +1,9 @@
-$(document).ready(function(){
-	
-	$('.collapsed').click(function(){
-		$('ul.navbar-right').removeClass('breadcrumb');
-		$('ul.navbar-right').addClass('nav');
-		$('ul.navbar-right').addClass('navbar-nav');
-	});
-});
+
+function collapseMenu(){
+		var classToAdd = " nav";
+		var otherClasstoAdd = " navbar-nav";
+		var x = document.getElementById('menu');
+		x.className = x.className.replace('breadcrumb','');
+		x.className = x.className + classToAdd;
+		x.className = x.className + otherClasstoAdd;
+}
